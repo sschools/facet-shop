2003a01a175d600dfef5adde50b3f3a6a603abcb
Brandon Aiton <brandonaiton@icloud.com>
2017-10-11 17:17:00 -0400

    more styling for cart

--- facet-shop-front-end/src/components/Cart.js
+++ facet-shop-front-end/src/components/Cart.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-
+import "../styles/Cart.css"
 import CartItem from "./CartItem"
 
 export default class Cart extends Component {
@@ -7,18 +7,18 @@ export default class Cart extends Component {
   render() {
     return (
       <div className="card bg-light shopping-cart">
-        <div className="card-header cart-header">
+        <div className="card-header cart-header d-flex">
           <i>shoping cart icon here</i>
           <h3>Shopping Cart</h3>
         </div>
         <div className="item-catcher">
-          <ul className="d-flex">
+          <ul className="d-flex cart-list-fix">
             <li className="list-group-item border-0 col-3">Product</li>
             <li className="list-group-item border-0 col-3">Price</li>
             <li className="list-group-item border-0 col-3">Quantity</li>
             <li className="list-group-item border-0 col-3">Remove</li>
           </ul>
-          
+          <hr className="separator-line"/>
           <CartItem/>
         </div>
         <div>
--- facet-shop-front-end/src/components/CartItem.js
+++ facet-shop-front-end/src/components/CartItem.js
@@ -5,7 +5,7 @@ export default class CartItem extends Component {
 
   render() {
     return (
-      <ul className="d-flex">
+      <ul className="d-flex cart-list-fix">
         <li className="list-group-item border-0 col-3">Product</li>
         <li className="list-group-item border-0 col-3">Price</li>
         <li className="list-group-item border-0 col-3">Quantity</li>
