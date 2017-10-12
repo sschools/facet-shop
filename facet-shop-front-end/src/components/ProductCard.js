f492c3e5f248998c35e36bbec519138338d6cb9f
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-12 15:30:32 -0400

    added map

--- facet-shop-front-end/src/components/ProductCard.js
+++ facet-shop-front-end/src/components/ProductCard.js
@@ -30,7 +30,7 @@ export default class ProductCard extends Component {
       <div className="card product-card">
          <img className="card-img-top w-10 h-10" src="http://sites.psu.edu/siowfa15/files/2015/10/stock-photo-intelligence-symbol-conceptual-design-creative-thinking-icon-isolated-on-white-background-126523163.jpg" alt="Card"/>
           <div className="card-body">
-            <h4 className="card-title product-name">Product Name:</h4>
+            <h4 className="card-title product-name">Product Name: {this.props.name}</h4>
             <p className="card-text">Price:</p>
             <form onSubmit={this._handleSubmit}>
               <input className="form-control" onChange={this._handleChange} placeholder="quantity"/>
